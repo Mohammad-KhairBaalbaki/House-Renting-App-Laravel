6e9130028b651e6f04490687ff0fe98a7bf1160c
Omar Kud <mirvat,swab@gmail.com>
2025-12-13 21:36:59 +0300

    add_search and_filters

--- House_Renting_App/app/Services/HouseService.php
+++ House_Renting_App/app/Services/HouseService.php
@@ -18,85 +18,87 @@ class HouseService
         $this->addressService = $addressService;
     }
 
-   public function index( $request)
-{
-    $houses = House::with('address.city.governorate')
-        ->where('is_active', true)
-        ->where('status_id', 2);
+    public function index($request)
+    {
+        $houses = House::with('address.city.governorate')
+            ->where('is_active', true)
+            ->where('status_id', 2);
 
-    if ($search = $request->input('search')) {
-        $houses->where(function ($q) use ($search) {
+        if ($search = $request->input('search')) {
+            $houses->where(function ($q) use ($search) {
 
-            $q->where('title', 'like', "%{$search}%")
-              ->orWhere('description', 'like', "%{$search}%")
-              ->orWhere('rooms', 'like', "%{$search}%") 
-              ->orWhere('space', 'like', "%{$search}%")
+                $q->where('title', 'like', "%{$search}%")
+                    ->orWhere('description', 'like', "%{$search}%")
+                    ->orWhere('rooms', 'like', "%{$search}%")
+                    ->orWhere('space', 'like', "%{$search}%")
 
 
-               ->orWhereHas('address.city', function ($city) use ($search) {
-                  $city->where('name->ar', 'like', "%{$search}%")
-                       ->orWhere('name->en', 'like', "%{$search}%");
-              })
-              ->orWhereHas('address.city.governorate', function ($gov) use ($search) {
-                  $gov->where('name->ar', 'like', "%{$search}%")
-                      ->orWhere('name->en', 'like', "%{$search}%");
-              });
-        });
+                    ->orWhereHas('address.city', function ($city) use ($search) {
+                        $city->where('name->ar', 'like', "%{$search}%")
+                            ->orWhere('name->en', 'like', "%{$search}%");
+                    })
+                    ->orWhereHas('address.city.governorate', function ($gov) use ($search) {
+                        $gov->where('name->ar', 'like', "%{$search}%")
+                            ->orWhere('name->en', 'like', "%{$search}%");
+                    });
+            });
+        }
+        if ($cityId = $request->input('city_id')) {
+            $houses->whereHas('address.city', function ($q) use ($cityId) {
+                $q->where('id', $cityId);
+            });
+        }
+
+        if ($govId = $request->input('governorate_id')) {
+            $houses->whereHas('address.city.governorate', function ($q) use ($govId) {
+                $q->where('id', $govId);
+            });
+        }
+        if ($Rent = $request->input('rent')) {
+            $houses->where('rent_value',  $Rent);
+        }
+        if ($minRent = $request->input('min_rent')) {
+            $houses->where('rent_value', '>=', $minRent);
+        }
+        if ($maxRent = $request->input('max_rent')) {
+            $houses->where('rent_value', '<=', $maxRent);
+        }
+
+        if ($minSpace = $request->input('min_space')) {
+            $houses->where('space', '>=', $minSpace);
+        }
+        if ($maxSpace = $request->input('max_space')) {
+            $houses->where('space', '<=', $maxSpace);
+        }
+
+        if ($minRooms = $request->input('min_rooms')) {
+            $houses->where('rooms', '>=', $minRooms);
+        }
+        if ($maxRooms = $request->input('max_rooms')) {
+            $houses->where('rooms', '<=', $maxRooms);
+        }
+        if ($rooms = $request->input('rooms')) {
+            $houses->where('rooms', $rooms);
+        }
+
+        $allowedSorts = ['rent_value', 'space', 'rooms', 'created_at'];
+        $sortBy = $request->input('sort_by', 'created_at');
+        $sortDir = $request->input('sort_dir', 'desc');
+
+        if (!in_array($sortBy, $allowedSorts)) {
+            $sortBy = 'created_at';
+        }
+
+        if (!in_array(strtolower($sortDir), ['asc', 'desc'])) {
+            $sortDir = 'desc';
+        }
+
+        $houses->orderBy($sortBy, $sortDir);
+
+        //  $houses = $houses->paginate(5);
+
+        return $houses->paginate(5);
     }
-    if ($cityId = $request->input('city_id')) {
-        $houses->whereHas('address.city', function ($q) use ($cityId) {
-            $q->where('id', $cityId);
-        });
-    }
-
-    if ($govId = $request->input('governorate_id')) {
-        $houses->whereHas('address.city.governorate', function ($q) use ($govId) {
-            $q->where('id', $govId);
-        });
-    }
-
-    if ($minRent = $request->input('min_rent')) {
-        $houses->where('rent_value', '>=', $minRent);
-    }
-    if ($maxRent = $request->input('max_rent')) {
-        $houses->where('rent_value', '<=', $maxRent);
-    }
-
-    if ($minSpace = $request->input('min_space')) {
-        $houses->where('space', '>=', $minSpace);
-    }
-    if ($maxSpace = $request->input('max_space')) {
-        $houses->where('space', '<=', $maxSpace);
-    }
-
-    if ($minRooms = $request->input('min_rooms')) {
-        $houses->where('rooms', '>=', $minRooms);
-    }
-    if ($maxRooms = $request->input('max_rooms')) {
-        $houses->where('rooms', '<=', $maxRooms);
-    }
-    if ($rooms = $request->input('rooms')) {
-        $houses->where('rooms', $rooms);
-    }
-
-    $allowedSorts = ['rent_value', 'space', 'rooms', 'created_at'];
-    $sortBy  = $request->input('sort_by', 'created_at');
-    $sortDir = $request->input('sort_dir', 'desc');
-
-    if (! in_array($sortBy, $allowedSorts)) {
-        $sortBy = 'created_at';
-    }
-
-    if (! in_array(strtolower($sortDir), ['asc', 'desc'])) {
-        $sortDir = 'desc';
-    }
-
-    $houses->orderBy($sortBy, $sortDir);
-
-  //  $houses = $houses->paginate(5);
-
-    return $houses->paginate(5);
-}
 
 
     public function store(array $data)
