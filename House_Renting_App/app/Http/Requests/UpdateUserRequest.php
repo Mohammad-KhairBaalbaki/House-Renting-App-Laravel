8160128d16353ac04b3eb1e8710bbbff8c26b850
Omar Kud <mirvat,swab@gmail.com>
2025-12-20 12:39:54 +0300

    finish_profile

--- House_Renting_App/app/Http/Requests/UpdateUserRequest.php
+++ House_Renting_App/app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -22,12 +23,34 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
+            "current_password"=>['required',"string"],
             'first_name' => ['sometimes', 'string'],
             'last_name' => ['sometimes', 'string'],
             'phone' => ['sometimes', 'string', 'unique:users,phone,'.$this->user()->id],
             'date_of_birth' => ['sometimes', 'date'],
-            'password' => ['sometimes', 'string', 'confirmed', 'min:8', 'max:20'],
+            'new_password' => ['sometimes', 'string', 'confirmed', 'min:8', 'max:20'],
             'profile_image'=>[ "sometimes",'file', 'mimes:jpeg,png,jpg,svg', 'max:2048'],
         ];
     }
+public function withValidator($validator)
+{
+    $validator->after(function ($validator) {
+
+        if (!$this->filled('current_password')) {
+            return;
+        }
+
+        if (!Hash::check($this->input('current_password'), $this->user()->password)) {
+            $validator->errors()->add('current_password', 'Current password is incorrect.');
+        }
+        $excluded = ['current_password', 'new_password_confirmation'];
+
+        $keys = collect($this->all())->keys()->diff($excluded);
+
+        if ($keys->isEmpty()) {
+            $validator->errors()->add('data', 'No data provided to update.');
+        
+      }
+    });
+}
 }
