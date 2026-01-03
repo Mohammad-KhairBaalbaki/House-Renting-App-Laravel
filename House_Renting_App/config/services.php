ad586602eca7d3a715865d3a84feea4e2cdc31f9
Omar Kud <mirvat,swab@gmail.com>
2026-01-03 18:13:41 +0300

    finish_notifacation

--- House_Renting_App/config/services.php
+++ House_Renting_App/config/services.php
@@ -13,6 +13,9 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
+    'fcm' => [
+    'enabled' => env('ENABLE_FCM', false),
+],
 
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
